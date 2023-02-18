408af4626fcd1b2290064c34bbbea41fd07403c0
achahine78 <achahine78@gmail.com>
2023-02-18 14:50:28 +0400

    feat: add list view on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Divider, List } from 'antd';
 import './App.css'
 
+const topicList = [
+  {
+    id: 0,
+    title: 'Topic 1'
+  },
+  {
+    id: 1,
+    title: 'Topic 2'
+  },
+  {
+    id: 2,
+    title: 'Topic 3'
+  },
+  {
+    id: 3,
+    title: 'Topic 4'
+  }
+];
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Divider orientation="left">Topic List</Divider>
+    <List
+      bordered
+      dataSource={topicList}
+      renderItem={(item) => (
+        <List.Item>
+          {item.title}
+        </List.Item>
+      )}
+    />
     </div>
   )
 }
