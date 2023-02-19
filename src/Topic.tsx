7dff10cdfc93d6b345c2617f497f5aeed1ea9599
achahine78 <achahine78@gmail.com>
2023-02-19 15:16:05 +0400

    feat: add icons to verify whether or not you got the right answer

--- src/Topic.tsx
+++ src/Topic.tsx
@@ -1,25 +1,26 @@
-import { Card, Collapse, Typography } from "antd";
+import { Card, Col, Collapse, Row } from "antd";
+import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import { useState } from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const questionsList = [
   {
     id: 0,
     question: "What is the output?",
     answer: "number",
-    isCorrectlyAnswered: false,
+    correctnessScore: 0,
   },
   {
     id: 1,
     question: "What is the output?",
     answer: "array",
-    isCorrectlyAnswered: false,
+    correctnessScore: 0,
   },
   {
     id: 2,
     question: "What is the output?",
     answer: "ReferenceError",
-    isCorrectlyAnswered: false,
+    correctnessScore: 0,
   },
 ];
 
@@ -27,14 +28,15 @@ type Question = {
   id: number;
   question: string;
   answer: string;
-  isCorrectlyAnswered: boolean;
+  correctnessScore: number;
 };
 
 type QuestionCardProps = {
   question: Question;
+  goToNextQuestion: () => void;
 };
 
-const QuestionCard = ({ question }: QuestionCardProps) => {
+const QuestionCard = ({ question, goToNextQuestion }: QuestionCardProps) => {
   return (
     <Card title={question.question} bordered={false} style={{ width: 300 }}>
       <Collapse bordered={false}>
@@ -42,17 +44,36 @@ const QuestionCard = ({ question }: QuestionCardProps) => {
           {question.answer}
         </Collapse.Panel>
       </Collapse>
+      <Row style={{ marginTop: '1em' }}>
+        <Col span={12} style={{ display: 'flex', justifyContent: 'center' }}>
+          <CheckCircleOutlined onClick={goToNextQuestion} style={{ fontSize: '2em', color: '#52c41a' }}/>
+        </Col>
+        <Col span={12} style={{ display: 'flex', justifyContent: 'center' }}>
+          <CloseCircleOutlined onClick={goToNextQuestion} style={{ fontSize: '2em', color: '#ff4d4f' }} />
+        </Col>
+      </Row>
     </Card>
   );
 };
 
 export const Topic = () => {
   const { id } = useParams();
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const sortedQuestions = questionsList.sort(
+    (a, b) => a.correctnessScore - b.correctnessScore
+  );
+  const currentQuestion = sortedQuestions[currentQuestionIndex];
   return (
     <div className="App">
-      {questionsList.map((question) => (
-        <QuestionCard question={question} />
-      ))}
+      <QuestionCard
+        question={currentQuestion}
+        goToNextQuestion={() => {
+          if (currentQuestionIndex === sortedQuestions.length - 1) return;
+          setCurrentQuestionIndex(
+            (currentQuestionIndex) => currentQuestionIndex + 1
+          );
+        }}
+      />
     </div>
   );
 };
